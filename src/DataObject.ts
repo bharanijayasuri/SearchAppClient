4a1a6530e80f4d4b84263e3456a85ae6da91f9c7
Bharani Jayasuri <44368021+bharanijayasuri@users.noreply.github.com>
2023-11-15 01:29:54 +0000

    client changes

---
+++ src/DataObject.ts
@@ -0,0 +1,7 @@
+export interface DataObject {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+  }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { DataObject } from '../DataObject';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'SearchAppClient';
+  title = 'SearchApp Client';
+  baseUrl = 'https://localhost:7206/api/Search/Search';
+  keyword :string = '';
+  queryUrl: string='';
+  dataReturned : DataObject[]=[];
+  showError: boolean = false;
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit(searchTerm: string) {
+    this.showError = false;
+    this.dataReturned.length = 0;
+    if(searchTerm.length == 0)
+    {
+      this.showError = true;
+      return;
+    }
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+    //let queryParams = new HttpParams();
+    // We have to append this way because HttParams objects are immutable ( can't be modified so we need a new one)
+    //queryParams = queryParams.append('searchString', searchTerm);
+    this.queryUrl = this.baseUrl+'?searchString='+searchTerm;
+    console.log(searchTerm);
+    this.http.post<DataObject[]>(this.queryUrl, httpOptions).subscribe(results => {
+      this.dataReturned = results;
+      console.log(this.dataReturned);
+    });
+  }
 }
+
